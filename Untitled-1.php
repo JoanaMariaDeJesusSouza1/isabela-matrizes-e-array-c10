3b6ec108f6e27fc28dba8ed510441e71b0e76523
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-02-03 15:38:36 -0300

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,42 @@
+//declarando variáveis
+
+//preload carrega as midías
+function preload(){
+ //animação do Trex
+
+
+  //imagem do solo
+
+}
+//setuo faz aconfiguração
+function setup(){
+  createCanvas(600,200)
+  // criando as bordas
+
+  //crie um sprite de trex
+ 
+  // adicione dimensão e posição ao trex
+ 
+  //sprite do solo
+  
+//draw faz o movimento, a ação do jogo
+function draw(){
+  background("#abedeb")
+ 
+  // fazero trex pular
+ 
+ 
+ //coordenadas do mouse na tela
+ //Text("X: "+mouseX+"  / Y: "+mouseY,mouseX,mouseY);
+  
+ // chamando a  função de gravidade
+
+  //colisão do trex com as bordas
+ 
+  
+ drawSprites();
+
+}
+// função de gravidade
+
+}
